0270f000cf17b6deace9d15ea11687e52422d072
Shiyi Chen <sychen@udel.edu>
2016-11-22 18:16:14 -0500

    todo list beta

--- TodoList/assets/js/todos.js
+++ TodoList/assets/js/todos.js
@@ -1,10 +1,23 @@
-$('li').on('click', function(){
+$('ul').on('click', 'li', function(){
 	$(this).toggleClass('completed');
 });
 
-$('span').on('click',function(event){
+$('ul').on('click', 'span', function(event){
 	$(this).parent().fadeOut(500, function(){ //$(this).parent() refer to li
 		$(this).remove(); //$(this) is now the li
 	});
 	event.stopPropagation();
+});
+
+$('input[type="text"]').keypress(function(event){
+	if (event.which === 13) {
+		//append the new todo string to ul
+		$('ul').append('<li><span class="fa fa-trash"></span> ' + $(this).val() + '</li>');
+		//remove the input text
+		$(this).val('');
+	}
+});
+
+$('.fa-plus').click(function(){
+	$('input').fadeToggle();
 });
