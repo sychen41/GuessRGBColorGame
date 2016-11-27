a284cf5a2fded2392a981693b7b37131fdcacb47
Shiyi Chen <sychen@udel.edu>
2016-11-27 21:09:45 +0000

    refactor to RESTful route

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,41 +1,80 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express     = require('express'),
+    app         = express(),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose');
 
+mongoose.connect('mongodb://localhost/yelp_camp');
+//mongoose.connect('mongodb://admin:admin@ds111748.mlab.com:11748/ycdb');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-var campgrounds = [
-    {name: '1camp', image: 'http://www.goodnewsnetwork.org/wp-content/uploads/2015/07/running-dog-CC-DouglasMWeston.jpg'},
-    {name: '2camp', image: 'https://files.graphiq.com/stories/t4/15_Tiniest_Dog_Breeds_1718_3083.jpg'},
-    {name: '3camp', image: 'https://i.ytimg.com/vi/6j7oQletFbc/maxresdefault.jpg'},
-    {name: '4camp', image: 'http://www.goodnewsnetwork.org/wp-content/uploads/2015/07/running-dog-CC-DouglasMWeston.jpg'},
-    {name: '5camp', image: 'https://files.graphiq.com/stories/t4/15_Tiniest_Dog_Breeds_1718_3083.jpg'},
-    {name: '6camp', image: 'https://i.ytimg.com/vi/6j7oQletFbc/maxresdefault.jpg'},
-    {name: '7camp', image: 'http://www.goodnewsnetwork.org/wp-content/uploads/2015/07/running-dog-CC-DouglasMWeston.jpg'},
-    {name: '8camp', image: 'https://files.graphiq.com/stories/t4/15_Tiniest_Dog_Breeds_1718_3083.jpg'},
-    {name: '9camp', image: 'https://i.ytimg.com/vi/6j7oQletFbc/maxresdefault.jpg'}
-];
+//just a pattern, still flexible
+var campSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+//create an object model with variables(like: name, image) and methods(like: CRUD methods)
+var Campground = mongoose.model('Campground', campSchema);
 
 app.get('/', function(req, res){
     res.render('landing');
 });
 
+//INDEX(目录) route: show all campgrounds
 app.get('/campgrounds', function(req, res){
-    res.render('campgrounds', {campgrounds: campgrounds});
+    Campground.find({},function(err, campgrounds){
+        if(!err) {
+            console.log('SUCCESS: retrieve data from db');
+            res.render('index', {campgrounds: campgrounds});
+        }
+        else {
+            console.log('FAILED: retrieve data from db');
+            console.log(err);
+        }
+    });
 });
 
+//NEW route: show form to create new campground
 app.get('/campgrounds/new', function(req, res){
-    res.render('newCampground');
+    res.render('new');
 });
 
-app.post('/campgrounds', function(req, res) {
-    campgrounds.push({
-        name: req.body.name,
-        image: req.body.image
-    });     
-    res.redirect('/campgrounds');
+//SHOW route: shows more info about a specific campground
+//note: this route MUST be after the NEW route, otherwise, 
+//the 'new' in /XXXX/new will be treated as an id
+app.get('/campgrounds/:id', function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if(!err) {
+            console.log('SUCCESS: retrieve the user-chosen campground from db');
+            res.render('show', {campground: foundCampground});
+        } else {
+            console.log('FAILED: retrieve the user-chosen campground from db');
+            console.log(err);
+        }
+    });
 });
+
+//CREATE route: add new campground to db
+app.post('/campgrounds', function(req, res) {
+    Campground.create({
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description
+    }, function(err, campg) {
+        if(!err) {
+            console.log('SUCCESS: new campground inserted to db');
+            console.log(campg);
+            res.redirect('/campgrounds');
+        } else {
+            console.log('FAILED: new campground inserted to db');
+            console.log(err);
+            //do sth to tell users and have them to post again
+        }
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('yelpcamp started');
