ba10f08487e397a5b21edd1fbfbb8759e0e75953
Shiyi Chen <sychen@udel.edu>
2016-11-28 04:41:41 +0000

    add SHOW route and some styling

--- BlogApp/app.js
+++ BlogApp/app.js
@@ -19,10 +19,12 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model('Blog', blogSchema);
 
+//home page
 app.get('/', function(req, res) {
     res.redirect('/blogs');
 });
 
+//INDEX
 app.get('/blogs', function(req, res) {
     Blog.find({}, function(err, blogs) {
         if(!err) {
@@ -35,10 +37,12 @@ app.get('/blogs', function(req, res) {
     });
 });
 
+//NEW
 app.get('/blogs/new', function(req, res){
     res.render('new');
 });
 
+//CREATE
 app.post('/blogs', function(req, res){
     //the way we setup the new, req.body.blog is already an object
     //so we don't need {title: req.body.title, image: req.......}
@@ -50,6 +54,19 @@ app.post('/blogs', function(req, res){
             console.log('FAILED: add new blog to db');
             console.log(err);
         } 
+    });
+});
+
+//SHOW
+app.get('/blogs/:id', function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(!err) {
+            console.log('SUCCESS: retrieve a specific blog from db');
+            res.render('show', {blog: foundBlog});
+        } else {
+            console.log('FAILED: retrieve a specific blog from db');
+            console.log(err);
+        }
     });
 });
 
