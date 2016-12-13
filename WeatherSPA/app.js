38de682a9e7ebdc6fb70e90dca77f8da456327f1
Shiyi Chen <sychen@udel.edu>
2016-12-13 00:49:26 -0500

    setup a NodeJS server

--- WeatherSPA/app.js
+++ WeatherSPA/app.js
@@ -1,10 +1,11 @@
-// MODULE
-var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.get('/', function(req, res){
+    res.render('index');
+});
 
-//Routes in routes.js
-
-//Services in services.js
-
-//CONTROLLERS in controllers.js
-
-//Directives in directives.js
+app.listen('8083', process.env.IP, function(){
+    console.log('Weather SPA started...');
+}); 
---
+++ WeatherSPA/public/js/script.js
@@ -0,0 +1,10 @@
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+//Routes in routes.js
+
+//Services in services.js
+
+//CONTROLLERS in controllers.js
+
+//Directives in directives.js
